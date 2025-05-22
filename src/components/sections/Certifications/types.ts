8180d508d93ccabaf6eedb4cacd5c2e05129ace8
asheeque <asheeque.cm@gmail.com>
2025-05-21 17:11:48 -0700

    refactiot other sections

--- src/components/sections/Certifications/types.ts
+++ src/components/sections/Certifications/types.ts
@@ -1,3 +1,14 @@
 import { Database } from '../../../types/supabase';
 
-export type Certifications = Database['public']['Tables']['certifications']['Row']; 
+export type Certifications = {
+  created_at: string;
+  date_earned: string | null;
+  expiration_date: string | null;
+  id: string;
+  issuer: string;
+  name: string;
+  sort_index: number;
+  tailoring_id: string;
+  credential_url?: string | null;
+  description?: string | null;
+}; 
--- src/components/sections/Projects/types.ts
+++ src/components/sections/Projects/types.ts
@@ -1,3 +1,17 @@
 import { Database } from '../../../types/supabase';
 
-export type Projects = Database['public']['Tables']['projects']['Row']; 
+export type Projects = {
+  created_at: string;
+  description: string | null;
+  end_date: string | null;
+  id: string;
+  link: string | null;
+  name: string;
+  role: string | null;
+  sort_index: number;
+  start_date: string | null;
+  tailoring_id: string;
+  bullets: string[];
+  url?: string | null;
+  github_url?: string | null;
+}; 
